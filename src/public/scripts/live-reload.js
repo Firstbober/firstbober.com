e9a32e8c71b1939cf93141d350f0411d51a32ef4
Firstbober <firstbober@tutanota.com>
2024-09-19 20:01:32 +0200

    src: fix live-reload

--- src/public/scripts/live-reload.js
+++ src/public/scripts/live-reload.js
@@ -1,6 +1,7 @@
 // NOTE: The server never fulfills this request, it stalls forever.
 // Once the connection is closed, we try to connect with the server until we establish a successful
 // connection. Then we reload the page.
-await fetch("/templator/watch").then(async () => {
+await fetch("/templator/watch").then(async (d) => {
+  if(d.status != 200) return;
   window.location.reload();
 });
